ab953eb73f73b26ecbd0f566e64f646960565d30
samuel-williams-ed <70590831+samuel-williams-ed@users.noreply.github.com>
2023-08-20 12:00:27 +0100

    Added Service layer to controller. Autowired in the Service component

--- demo/src/main/java/com/example/demo/grouce/controller/GrouceController.java
+++ demo/src/main/java/com/example/demo/grouce/controller/GrouceController.java
@@ -1,13 +1,24 @@
 package com.example.demo.grouce.controller;
 
+import com.example.demo.grouce.services.GrouceService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(path = "api/v1/")
 public class GrouceController {
+
+    private final GrouceService grouceService;
+
+    @Autowired
+    public GrouceController(GrouceService grouceService) {
+        this.grouceService = grouceService;
+    }
 
     @GetMapping
     public String getHome(){
-        return "Welcome";
+        return grouceService.getHome();
     }
 }
