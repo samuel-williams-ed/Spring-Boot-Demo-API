c6d8b282d2b72bb3a1f7e97ac0c513d4c2a2cd09
samuel-williams-ed <70590831+samuel-williams-ed@users.noreply.github.com>
2023-08-20 11:30:48 +0100

    Initial commit

---
+++ demo/src/main/java/com/example/demo/grouce/GrouceApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo.grouce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrouceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrouceApplication.class, args);
+	}
+
+}
---
+++ demo/src/test/java/com/example/demo/grouce/GrouceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo.grouce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrouceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
